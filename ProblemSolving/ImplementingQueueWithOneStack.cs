157c362b53494352c9cc2c7d1d03b45092454e18
Shahd Salama <Shahd.Salama@itworx.com>
2021-11-05 14:11:48 +0200

    implementing queue with one stack using recursion

--- ProblemSolving/ImplementingQueueWithOneStack.cs
+++ ProblemSolving/ImplementingQueueWithOneStack.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace HackerRank
 {
-    class ImplementingQueueWithStacks
+
+    // class of queue having two stacks
+    public class Queue
     {
-        // class of queue having two stacks
-        public class Queue
+        public Queue()
         {
-            public Stack<int> stack1;
+            stack1 = new Stack<int>();
         }
+
+        public Stack<int> stack1;
 
         /* Function to push an item to stack*/
         static void push(Stack<int> top_ref, int new_data)
@@ -33,61 +35,61 @@ namespace HackerRank
         }
 
         /* Function to enqueue an item to queue */
-        static void enQueue(Queue q, int x)
+        public void enQueue(int x)
         {
-            push(q.stack1, x);
+            push(stack1, x);
         }
 
         /* Function to deQueue an item from queue */
-        static int deQueue(Queue q)
+        public int deQueue()
         {
             int x, res = 0;
 
             /* If the stacks is empty then error */
-            if (q.stack1.Count == 0)
+            if (stack1.Count == 0)
             {
                 Console.WriteLine("Q is Empty");
                 Environment.Exit(0);
             }
 
             // Check if it is a last element of stack
-            else if (q.stack1.Count == 1)
+            else if (stack1.Count == 1)
             {
-                return pop(q.stack1);
+                return pop(stack1);
             }
             else
             {
 
                 /* pop an item from the stack1 */
-                x = pop(q.stack1);
+                x = pop(stack1);
 
                 /* store the last deQueued item */
-                res = deQueue(q);
+                res = deQueue();
 
                 /* push everything back to stack1 */
-                push(q.stack1, x);
+                push(stack1, x);
                 return res;
             }
             return 0;
         }
-
-        /* Driver function to test above functions */
-        public static void Main(String[] args)
-        {
-            /* Create a queue with items 1 2 3*/
-            Queue q = new Queue();
-            q.stack1 = new Stack<int>();
-
-            enQueue(q, 1);
-            enQueue(q, 2);
-            enQueue(q, 3);
-
-            /* Dequeue items */
-            Console.Write(deQueue(q) + " ");
-            Console.Write(deQueue(q) + " ");
-            Console.Write(deQueue(q) + " ");
-        }
-
-
     }
+
+    ///* Driver function to test above functions */
+
+    ///* Create a queue with items 1 2 3*/
+    //Queue q = new Queue();
+    //q.stack1 = new Stack<int>();
+
+    //    enQueue(q, 1);
+    //enQueue(q, 2);
+    //enQueue(q, 3);
+
+    ///* Dequeue items */
+    //Console.Write(deQueue(q) + " ");
+    //    Console.Write(deQueue(q) + " ");
+    //    Console.Write(deQueue(q) + " ");
+
+
+
+
 }
--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -8,19 +8,27 @@ namespace HackerRank
     {
         public static void Main(string[] args)
         {
-            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-            int T = Convert.ToInt32(Console.ReadLine().Trim());
+            //    //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            //    int T = Convert.ToInt32(Console.ReadLine().Trim());
 
-            for (int TItr = 0; TItr < T; TItr++)
-            {
-                string w = Console.ReadLine();
+            //    for (int TItr = 0; TItr < T; TItr++)
+            //    {
+            //        string w = Console.ReadLine();
 
-                string result = BiggerisGreater.biggerIsGreater(w);
+            //        string result = BiggerisGreater.biggerIsGreater(w);
 
-               Console.WriteLine(result);
-            }
+            //       Console.WriteLine(result);
+            //    }
 
+            Queue q = new Queue();
+            q.enQueue(1);
+            q.enQueue(2);
+            q.enQueue(3);
 
+            /* Dequeue items */
+            Console.Write(q.deQueue() + " ");
+            Console.Write(q.deQueue() + " ");
+            Console.Write(q.deQueue() + " ");
             //textWriter.WriteLine(result);
 
             //textWriter.Flush();
