748653d73c062ca057473ca763d71170bf8b6315
Shahd Salama <Shahd.Salama@itworx.com>
2021-11-07 15:05:16 +0200

    cleaning up code

--- ProblemSolving/ReverseSinglyLinkedList.cs
+++ ProblemSolving/ReverseSinglyLinkedList.cs
@@ -1,15 +1,4 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
+﻿
 using System;
 
 class Solution
@@ -54,47 +43,6 @@ class Solution
             this.tail = node;
         }
     }
-
-    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep)
-    {
-        while (node != null)
-        {
-            Console.Write(node.data);
-
-            node = node.next;
-
-            if (node != null)
-            {
-                Console.Write(sep);
-            }
-        }
-    }
-
-    class Result
-    {
-
-        /*
-         * Complete the 'reversePrint' function below.
-         *
-         * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
-         */
-
-        /*
-         * For your reference:
-         *
-         * SinglyLinkedListNode
-         * {
-         *     int data;
-         *     SinglyLinkedListNode next;
-         * }
-         *
-         */
-
-
-    }
-
-
-
 
     public static void reversePrint(SinglyLinkedListNode llist)
     {
