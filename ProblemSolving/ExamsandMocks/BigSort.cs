2f8c67a5c29e6329d15ce57d7922755c70a5025f
Shahd Salama <Shahd.Salama@itworx.com>
2022-01-24 11:29:07 +0200

    rearranging

---
+++ ProblemSolving/ExamsandMocks/BigSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.ExamsandMocks
+{
+    public class CustomComparer : IComparer<string>
+    {
+        public int Compare(string a, string b)
+        {
+            if (a.Length != b.Length) return a.Length - b.Length;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                var left = a[i];
+                var right = b[i];
+                if (left != right) return left - right;
+            }
+            return 0;
+        }
+    }
+
+
+    public class BigSort
+    {
+        public static List<string> bigSorting(List<string> unsorted)
+        {
+            unsorted.Sort(new CustomComparer());
+
+            return unsorted;
+
+
+
+        }
+
+    }
+}
